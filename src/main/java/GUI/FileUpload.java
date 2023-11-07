f187bc987870cd3077c3fe2a54239551955c6bf1
gfavale1 <favalegiacomo25@outlook.com>
2023-11-07 14:28:07 +0100

    Inserimento della classe MIDIAnalysis

--- src/main/java/GUI/FileUpload.java
+++ src/main/java/GUI/FileUpload.java
@@ -7,22 +7,38 @@ import java.io.File;
 public class FileUpload extends JFrame {
     private JLabel label = new JLabel("Inserisci due track MIDI:");
     private JButton button = new JButton("Upload");
-    private JTextField filePathTextField = new JTextField();
     private JPanel panel = new JPanel();
     public FileUpload () {
-        button.addActionListener(e -> {
-                JFileChooser fileChooser = new JFileChooser();
-                int result = fileChooser.showOpenDialog(null);
-                if (result == JFileChooser.APPROVE_OPTION) {
-                    File selectedFile = fileChooser.getSelectedFile();
-                    String path = selectedFile.getAbsolutePath();
-                    filePathTextField.setText(path);
-                }
-        });
-
         panel.add(label);
         panel.add(button);
-        panel.add(filePathTextField);
         this.add(panel, BorderLayout.CENTER);
+
+        button.addActionListener(e -> {
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setMultiSelectionEnabled(true);
+            int result = fileChooser.showOpenDialog(null);
+            if (result == JFileChooser.APPROVE_OPTION) {
+                File[] selectedFiles = fileChooser.getSelectedFiles();
+                try {
+                    if (selectedFiles.length >= 2) {
+                        String path1 = selectedFiles[0].getAbsolutePath();
+                        String path2 = selectedFiles[1].getAbsolutePath();
+                        MIDIAnalysis midiAnalysis = new MIDIAnalysis(path1, path2);
+                        midiAnalysis.setDefaultCloseOperation(EXIT_ON_CLOSE);
+                        midiAnalysis.setTitle("Carica");
+                        midiAnalysis.setSize(400, 400);
+                        midiAnalysis.setResizable(false);
+                        midiAnalysis.setVisible(true);
+                        this.setVisible(false);
+                    }
+                } catch (Exception exception) {
+
+                }
+            }
+        });
+
+
+
+
     }
 }
